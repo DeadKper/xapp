c0543863e027ef3c4fc6810503f1c155b57ef4ac
Missael Gatelum <35360914+DeadKper@users.noreply.github.com>
2022-11-28 16:56:14 -0700

    add sudo loop to prevent asking for password

--- xdata/__init__.py
+++ xdata/__init__.py
@@ -4,7 +4,7 @@ from xdata.item import Item
 from xdata.item_dict import ItemDict, EMPTY_ITEM_DICT
 from xdata.package_manager import PackageManager
 from xdata.frozen_dict import FrozenDict
-from xdata.methods import DEFAULT, ERROR, WARNING, error
+from xdata.methods import DEFAULT, ERROR, WARNING, error, sudoloop
 
 __all__ = ['Color', 'ManagerInfo', 'Item',
-           'ItemDict', 'PackageManager', 'FrozenDict', 'DEFAULT', 'ERROR', 'WARNING', 'error', 'EMPTY_ITEM_DICT']
+           'ItemDict', 'PackageManager', 'FrozenDict', 'DEFAULT', 'ERROR', 'WARNING', 'error', 'EMPTY_ITEM_DICT', 'sudoloop']
--- xdata/methods.py
+++ xdata/methods.py
@@ -1,6 +1,9 @@
 from re import sub as sed
 from xdata import Color
 from sys import stderr
+from subprocess import Popen, PIPE
+from threading import Thread
+from time import sleep
 
 DEFAULT = 0
 ERROR = 1
@@ -20,3 +23,22 @@ def error(text: str, type=DEFAULT, code=ERROR):
         text = f'{Color.BOLD}{Color.RED}Error{Color.END}: {text}'
     print(f'{lines}{text}', file=stderr)
     quit(code)
+
+
+run_sudo_loop = False
+sudo_loop_thread: Thread
+
+
+def _sudo_loop():
+    while run_sudo_loop:
+        sleep(30)
+        Popen(['sudo', 'echo', '""'], stdout=PIPE, stderr=PIPE)
+
+
+def sudoloop(run=True):
+    global run_sudo_loop
+    global sudo_loop_thread
+    run_sudo_loop = run
+    if run and not sudo_loop_thread.is_alive():
+        sudo_loop_thread = Thread(target=_sudo_loop)
+        sudo_loop_thread.run()
