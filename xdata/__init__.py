41491159c6563bbde19d4dd4f605ef8f07233705
Missael Gatelum <35360914+DeadKper@users.noreply.github.com>
2022-11-28 17:20:19 -0700

    solve import error

--- xdata/__init__.py
+++ xdata/__init__.py
@@ -2,9 +2,9 @@ from xdata.color import Color
 from xdata.manager_info import ManagerInfo
 from xdata.item import Item
 from xdata.item_dict import ItemDict, EMPTY_ITEM_DICT
-from xdata.package_manager import PackageManager
 from xdata.frozen_dict import FrozenDict
 from xdata.methods import DEFAULT, ERROR, WARNING, error, sudoloop
+from xdata.package_manager import PackageManager
 
 __all__ = ['Color', 'ManagerInfo', 'Item',
            'ItemDict', 'PackageManager', 'FrozenDict', 'DEFAULT', 'ERROR', 'WARNING', 'error', 'EMPTY_ITEM_DICT', 'sudoloop']
