72a5e642a8338c64656d707fa01758673628a794
Missael Gatelum <35360914+DeadKper@users.noreply.github.com>
2022-12-01 17:28:56 -0700

    fix empty dict

--- xdata/items/__init__.py
+++ xdata/items/__init__.py
@@ -2,6 +2,6 @@ from xdata.items.info import Info
 from xdata.items.item import Item
 from xdata.items.dict import Dict
 
-EMPTY_DICT = Dict([], 0)
+EMPTY_DICT = Dict([])
 
 __all__ = ['Info', 'Item', 'Dict', 'EMPTY_DICT']
